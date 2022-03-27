749247c9f2ebe3966dd0d7acdd2cd183f7f78770
Pouya <pouyajoon@gmail.com>
2022-03-28 01:54:58 +0200

    update server

--- src/server/server/server.ts
+++ src/server/server/server.ts
@@ -22,7 +22,9 @@ export function expressServer() {
     app.use(express.static(frontDistDir));
     app.use(express.urlencoded({ limit: '5mb', extended: true }));
     app.use(express.json({ limit: '5mb' }));
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+    if (process.env.NODE_ENV === 'production') {
+        app.use(enforce.HTTPS({ trustProtoHeader: true }));
+    }
     const port = process.env.PORT || 443;
     console.log("SETUP SERVER ON".blue, port, process.env.NODE_ENV, process.env.JUP_ENV);
     const server = getServer(app);
